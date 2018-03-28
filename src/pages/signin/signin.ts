e76c7ceeb106f3c088584aa7ef4f0d28bdc6fe40
CrisKrus <cristian.suarez.ulgpc@gmail.com>
2018-03-28 11:16:41 +0100

    Create basic user

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController} from 'ionic-angular';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {UserProvider} from "../../providers/user/user";
+import {HomePage} from "../home/home";
 
 @IonicPage()
 @Component({
@@ -7,12 +10,55 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'signin.html',
 })
 export class SigninPage {
+  user: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder, private userProvider: UserProvider) {
+    this.addInputValidators();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SigninPage');
+  addInputValidators() {
+    this.user = this.formBuilder.group({
+      name: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern('[a-zA-Z ]*'),
+        Validators.minLength(10),
+        Validators.maxLength(50)
+      ])],
+      gender: ['', Validators.compose([
+        Validators.required
+      ])],
+      birthday: ['', Validators.compose([
+        Validators.required
+      ])],
+      city: ['', Validators.compose([
+        Validators.required
+      ])],
+      phone: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern('[0-9]{9}'),
+      ])],
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.email
+      ])],
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(8)
+      ])]
+    });
+  }
+
+  submit(userFields: any): void{
+    if (this.user.valid && this.isNotRegisterJet(userFields.email)){
+
+      console.log(userFields);
+      //TODO make a register on data base
+      this.navCtrl.setRoot(HomePage, userFields);
+    }
+  }
+
+  private isNotRegisterJet(email: string) {
+    return this.userProvider.getUser(email.toLowerCase()) == undefined;
   }
 
 }
