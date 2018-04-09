98e1bdc49ef810ea0dc9a554a8cb7951166466a3
CrisKrus <cristian.suarez.ulgpc@gmail.com>
2018-04-09 19:42:29 +0100

    Hotfix, delete unnecessary files

--- src/components/components.module.ts
+++
@@ -1,9 +0,0 @@
-import {NgModule} from '@angular/core';
-import {NavbarTabsComponent} from './navbar-tabs/navbar-tabs';
-
-@NgModule({
-	declarations: [NavbarTabsComponent],
-	imports: [],
-	exports: [NavbarTabsComponent]
-})
-export class ComponentsModule {}
