4826cd23a434fcc359f19fd3b7b964a019396697
CrisKrus <cristian.suarez.ulgpc@gmail.com>
2018-04-11 12:33:21 +0100

    Add checkbox left side

--- src/pages/search-exercise/search-exercise.ts
+++ src/pages/search-exercise/search-exercise.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {ExercisesProvider} from "../../providers/exercises/exercises";
+import {NavController} from "ionic-angular";
+import {ViewExercisePage} from "../view-exercise/view-exercise";
 
 @Component({
   selector: 'page-search-exercise',
@@ -9,12 +11,13 @@ export class SearchExercisePage {
   private exercises;
   private exercisesShowingList: string[];
 
-  constructor(private exerciseProvider: ExercisesProvider) {
+  constructor(private navCtrl: NavController, private exerciseProvider: ExercisesProvider) {
     this.exercises = exerciseProvider.getExercises();
     this.initializeShowingListExercises();
   }
 
   private initializeShowingListExercises() {
+    //TODO add checkbox mark if exercise is assign to this patient
     this.exercisesShowingList = [];
     for (let key in this.exercises) {
       this.exercisesShowingList.push(this.exercises[key]);
@@ -46,6 +49,9 @@ export class SearchExercisePage {
   }
 
   exerciseSelected(exercise: any){
-    console.log('Exercise selected, ', exercise);
+    //TODO control if is doctor who view exercise or patient
+    //TODO when doctor assign exercise can add observations
+    //TODO observations have to be on patient history not on exercise data, bc each observations is different fon each patient
+    this.navCtrl.push(ViewExercisePage, exercise);
   }
 }
