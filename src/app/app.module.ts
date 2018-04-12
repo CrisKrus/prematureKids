d6afa08f7e698af6b24a5efd06ca0385d5c57754
CrisKrus <cristian.suarez.ulpgc@gmail.com>
2018-04-12 11:48:09 +0100

    Add firebase and angular fire dependencies and configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
 import {LoginPage} from '../pages/login/login';
-import { UserProvider } from '../providers/user/user';
+import {UserProvider} from '../providers/user/user';
 import {HttpClientModule} from "@angular/common/http";
 import {SigningUpPage} from "../pages/signing-up/signing-up";
-import { MunicipalitiesProvider } from '../providers/municipalities/municipalities';
+import {MunicipalitiesProvider} from '../providers/municipalities/municipalities';
 import {ViewProfilePage} from "../pages/view-profile/view-profile";
 import {TabsComponent} from "../pages/tabs/tabs";
 import {SearchPatientPage} from "../pages/search-patient/search-patient";
-import { ExercisesProvider } from '../providers/exercises/exercises';
+import {ExercisesProvider} from '../providers/exercises/exercises';
 import {ViewExercisePage} from "../pages/view-exercise/view-exercise";
 import {SearchExercisePage} from "../pages/search-exercise/search-exercise";
+import {AngularFireAuthModule} from "angularfire2/auth";
+import {AngularFireDatabaseModule} from "angularfire2/database";
+import {AngularFireModule} from "angularfire2";
+import {enviroment} from "../enviroments/enviroments";
 
 @NgModule({
   declarations: [
@@ -33,7 +37,11 @@ import {SearchExercisePage} from "../pages/search-exercise/search-exercise";
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    //TODO http client module is really in use?
     HttpClientModule,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(enviroment.firebase)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/enviroments/enviroments.ts
@@ -0,0 +1,11 @@
+export const enviroment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCMKxsN-fqj1SGyYUjm5g1qR6Eb3OeSuM4",
+        authDomain: "premature-kids.firebaseapp.com",
+        databaseURL: "https://premature-kids.firebaseio.com",
+        projectId: "premature-kids",
+        storageBucket: "premature-kids.appspot.com",
+        messagingSenderId: "817468465792"
+    }
+};
