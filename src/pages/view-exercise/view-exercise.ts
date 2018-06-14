fb27bffd378b5c82c79e19a7a8d4c241386d952a
criskrus <cristian.suarez.ulpgc@gmail.com>
2018-06-14 18:57:19 +0100

    Add images and video to exercise preview

--- src/pages/view-exercise/view-exercise.ts
+++ src/pages/view-exercise/view-exercise.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {UserProvider} from "../../providers/user/user";
+import * as firebase from "firebase";
 
 @Component({
   selector: 'page-view-exercise',
@@ -10,6 +11,8 @@ export class ViewExercisePage {
   private userSessionType;
   private exercise;
   protected textAreaInput: string;
+  private images = [];
+  private video = [];
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,  // navParams are use it on html directly
@@ -21,15 +24,46 @@ export class ViewExercisePage {
     this.textAreaInput = this.exercise.observations || "";
   }
 
-  isPatient(){
+  ionViewWillEnter() {
+    if (this.haveMediaTheExercise() && this.haveImagesTheExercise()) {
+      for (let mediaID of this.exercise.media.images) {
+        firebase.storage().ref('/exercises/' + this.exercise.id + '/' + mediaID).getDownloadURL().then((url) => {
+          this.images.push(url);
+        });
+      }
+    }
+
+    if (this.haveMediaTheExercise() && this.haveVideoTheExercise()) {
+      for (let mediaID of this.exercise.media.video) {
+        firebase.storage().ref('/exercises/' + this.exercise.id + '/' + mediaID).getDownloadURL().then((url) => {
+          this.video.push(url);
+        });
+      }
+    }
+
+  }
+
+  private haveImagesTheExercise() {
+    return this.exercise.media.images != undefined;
+  }
+
+  private haveVideoTheExercise() {
+    return this.exercise.media.video != undefined;
+  }
+
+  private haveMediaTheExercise() {
+    return this.exercise.media != undefined;
+  }
+
+  isPatient() {
     return this.userSessionType == 'patient';
   }
 
-  isDoctor(){
+  isDoctor() {
     return this.userSessionType == 'doctor';
   }
 
-  hasObservations(){
+  hasObservations() {
     return this.exercise.observations != undefined && this.exercise.observations != '';
   }
 
@@ -40,8 +74,10 @@ export class ViewExercisePage {
       });
   }
 
-  assignExerciseWithObservations(){
+  assignExerciseWithObservations() {
     this.userProvider.assignExercise(this.exercise.id, this.navParams.get('userId'), this.textAreaInput)
-      .then(() => {this.navCtrl.pop()});
+      .then(() => {
+        this.navCtrl.pop()
+      });
   }
 }
