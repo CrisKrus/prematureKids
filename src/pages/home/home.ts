a06b3a90a04b3f1e043a9a079d8cd405d4e0e8ba
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2018-07-09 13:09:58 +0100

    Refactor, move initialization of variables to ionViewWillEnter method

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,84 +6,89 @@ import {ViewProfilePage} from "../view-profile/view-profile";
 import {UserProvider} from "../../providers/user/user";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
-  private user: any | null;
-  protected isDoctor: boolean;
-  protected patients = [];
-  protected exercisesNotDone = [];
-  protected exerciseDone = [];
+    private user: any | null;
+    protected isDoctor: boolean;
+    protected patients = [];
+    protected exercisesNotDone = [];
+    protected exerciseDone = [];
 
-  constructor(public navCtrl: NavController,
-              private userProvider: UserProvider,
-              private exercisesProvider: ExercisesProvider) {
-    userProvider.getUser(userProvider.uid).then((user) => {
-      this.user = user;
-      this.setUserType();
-      this.setDataToShow();
-    });
-  }
-
-  private setUserType() {
-    this.isDoctor = (this.user.type == 'doctor');
-  }
-
-  private setDataToShow() {
-    if (this.isDoctor) {
-      this.setPatients(this.user['patients']);
-    } else {
-      this.setExercises(this.user['exercises']);
+    constructor(public navCtrl: NavController,
+                private userProvider: UserProvider,
+                private exercisesProvider: ExercisesProvider) {
     }
-  }
 
-  private setPatients(patientsUid) {
-    for (let uid in patientsUid) {
-      this.userProvider.getUser(uid).then((user) => {
-        //TODO this is bullshit
-        user['id'] = uid;
-        user['news'] = false;
-        for (let exerciseID in user['exercises']) {
-          if (this.userProvider.isExerciseDone(user['exercises'][exerciseID])) {
-            if (this.wasExerciseView(user['exercises'][exerciseID], uid)) {
-              user['news'] = true;
-              break;
-            }
-          }
+    ionViewWillEnter() {
+        this.userProvider.getUser(this.userProvider.uid).then((user) => {
+            this.user = user;
+            this.setUserType();
+            this.setDataToShow();
+        });
+    }
+
+    private setUserType() {
+        this.isDoctor = (this.user.type == 'doctor');
+    }
+
+    private setDataToShow() {
+        if (this.isDoctor) {
+            this.setPatients(this.user['patients']);
+        } else {
+            this.setExercises(this.user['exercises']);
         }
-        this.patients.push(user);
-      });
     }
-  }
 
-  private wasExerciseView(exercise, uid) {
-    return exercise['done'] > this.user['patients'][uid];
-  }
+    private setPatients(patientsUid) {
+        for (let uid in patientsUid) {
+            this.userProvider.getUser(uid).then((user) => {
+                //TODO this is bullshit
+                user['id'] = uid;
+                user['news'] = false;
+                for (let exerciseID in user['exercises']) {
+                    if (this.userProvider.isExerciseDone(user['exercises'][exerciseID])) {
+                        if (this.wasExerciseView(user['exercises'][exerciseID], uid)) {
+                            user['news'] = true;
+                            break;
+                        }
+                    }
+                }
+                this.patients.push(user);
+            });
+        }
+    }
+
+    private wasExerciseView(exercise, uid) {
+        return exercise['done'] > this.user['patients'][uid];
+    }
 
 //TODO refactor this big thing...
-  private setExercises(patientAssignedExercises) {
-    for (let exerciseID in patientAssignedExercises) {
-      this.exercisesProvider.getExercise(exerciseID).then((exercise) => {
-        //TODO this is bullshit
-        let assignedExercise = patientAssignedExercises[exerciseID];
-        exercise['id'] = exerciseID;
-        exercise['observations'] = assignedExercise.observations || "";
-        if (this.userProvider.isExerciseDone(assignedExercise)) {
-          this.exerciseDone.push(exercise);
-        } else {
-          this.exercisesNotDone.push(exercise);
+    private setExercises(patientAssignedExercises) {
+        this.exerciseDone = [];
+        this.exercisesNotDone = [];
+        for (let exerciseID in patientAssignedExercises) {
+            this.exercisesProvider.getExercise(exerciseID).then((exercise) => {
+                //TODO this is bullshit
+                let assignedExercise = patientAssignedExercises[exerciseID];
+                exercise['id'] = exerciseID;
+                exercise['observations'] = assignedExercise.observations || "";
+                if (this.userProvider.isExerciseDone(assignedExercise)) {
+                    this.exerciseDone.push(exercise);
+                } else {
+                    this.exercisesNotDone.push(exercise);
+                }
+            });
         }
-      });
     }
-  }
 
-  exerciseSelected(exercise: any) {
-    this.navCtrl.push(ViewExercisePage, {"exercise": exercise});
-  }
+    exerciseSelected(exercise: any) {
+        this.navCtrl.push(ViewExercisePage, {"exercise": exercise});
+    }
 
-  patientSelected(patient) {
-    this.userProvider.updateTimestampLastViewPatient(this.userProvider.uid, patient.id);
-    this.navCtrl.push(ViewProfilePage, {user: patient})
-  }
+    patientSelected(patient) {
+        this.userProvider.updateTimestampLastViewPatient(this.userProvider.uid, patient.id);
+        this.navCtrl.push(ViewProfilePage, {user: patient})
+    }
 }
