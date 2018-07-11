89688edfa9281c8c7c5fbe47848087313c6c9503
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2018-07-11 20:11:12 +0100

    Edit profile

---
+++ src/pages/edit-profile/edit-profile.ts
@@ -0,0 +1,77 @@
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MunicipalitiesProvider} from "../../providers/municipalities/municipalities";
+import {UserProvider} from "../../providers/user/user";
+
+@Component({
+    selector: 'page-edit-profile',
+    templateUrl: 'edit-profile.html',
+})
+export class EditProfilePage {
+    user: FormGroup;
+    protected municipalities: any;
+
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                public formBuilder: FormBuilder,
+                public municipalitiesProvider: MunicipalitiesProvider,
+                public userProvider: UserProvider) {
+        this.municipalities = this.municipalitiesProvider.getMunicipalities();
+        this.addInputValidators();
+    }
+
+    ionViewDidLoad() {
+        let user = this.navParams.get('user');
+        this.user.setValue({
+            city: user.city,
+            day: user.day,
+            email: user.email,
+            gender: user.gender,
+            month: user.month,
+            name: user.name,
+            phone: user.phone,
+            year: user.year
+        });
+    }
+
+    addInputValidators() {
+        this.user = this.formBuilder.group({
+            name: ['', Validators.compose([
+                Validators.required,
+                Validators.pattern('[a-zA-Z ]*'),
+                Validators.minLength(10),
+                Validators.maxLength(50)
+            ])],
+            gender: ['', Validators.compose([
+                Validators.required
+            ])],
+            day: ['', Validators.compose([
+                Validators.required
+            ])],
+            month: ['', Validators.compose([
+                Validators.required
+            ])],
+            year: ['', Validators.compose([
+                Validators.required
+            ])],
+            city: ['', Validators.compose([
+                Validators.required
+            ])],
+            phone: ['', Validators.compose([
+                Validators.required,
+                Validators.pattern('[0-9]{9}'),
+            ])],
+            email: ['', Validators.compose([
+                Validators.required,
+                Validators.email
+            ])],
+        });
+    }
+
+    saveProfileChanges(userData) {
+        this.userProvider
+            .editProfileData(userData, this.userProvider.uid)
+            .then(() => this.navCtrl.pop());
+    }
+}
