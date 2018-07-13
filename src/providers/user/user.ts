ecbfde0b767752a73fe97c63d124dbe9c7ead112
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2018-07-13 20:31:40 +0100

    Fix minor errors

--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -14,6 +14,7 @@ export class UserProvider {
         return this.angularFireAuth.auth.createUserWithEmailAndPassword(userData.email, userData.password)
             .then((user) => {
                 delete userData.password;
+                delete userData.password_confirmation; //todo test that
                 this.setUserData(user, userData);
                 return Promise.resolve(user);
             })
